c4aabd7de83435bb508dc2e6ea7d293733837ab1
Artem Zubkov <zubkov.artem46@gmail.com>
2021-11-09 00:28:26 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [streamStopwatch, setStreamStopwatch] = useState(false);
+
+  useEffect(() => {
+
+    const unsubscribe = new Subject();
+    interval(1)
+        .pipe(takeUntil(unsubscribe))
+        .subscribe(() => {
+          if (streamStopwatch) {
+            setTime(val => val + 1);
+          }
+        });
+    return () => {
+      unsubscribe.next();
+      unsubscribe.complete();
+    };
+  }, [streamStopwatch]);
+
+  const handleStart = () => {
+    setStreamStopwatch(prevState => !prevState);
+    
+  }
+
+
+  const handleResume = () => {
+    handleStart();
+  }
+
+
+  const handleStop = () => {
+    if (time !== 0) {
+      setStreamStopwatch(false);
+    }
+ 
+  }
+
+
+  const handleReset = () => {
+    setTime(0);
+    setStreamStopwatch(false);
+ 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Test task React + RxJS stopwatch</h4>
+      <p>{time}</p>
+
+      <div>
+        <button onClick={handleStart} >Start</button>
+        <button onClick={handleResume} >Resume</button>
+        <button onClick={handleReset} >Reset</button>
+        <button onClick={handleStop} >Stop</button>
+      </div>
     </div>
   );
 }
